f8f23e5ed74f6bf2e530dd1cc2659962bcd9e1bf
陈欣 <95323362@qq.com>
2016-06-08 13:14:24 +0800

    -

---
+++ L1-021. 重要的话说三遍.cpp
@@ -0,0 +1,23 @@
+L1-021. 重要的话说三遍
+
+时间限制
+400 ms
+内存限制
+65536 kB
+代码长度限制
+8000 B
+判题程序
+Standard
+作者
+陈越
+这道超级简单的题目没有任何输入。
+
+你只需要把这句很重要的话 —— “I'm gonna WIN!”'——连续输出三遍就可以了。
+
+注意每遍占一行，除了每行的回车不能有任何多余字符。
+#include <iostream>
+using namespace std;
+int main() {
+	cout << "I'm gonna WIN!" << endl << "I'm gonna WIN!" << endl <<  "I'm gonna WIN!";
+	return 0;
+}
